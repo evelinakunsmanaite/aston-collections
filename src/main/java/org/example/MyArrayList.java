f39d188b98fee122184274a45ab8786befca27b3
Evelina Kunsmanaite <evelinakunsmanaite@gmail.com>
2024-04-05 21:12:09 +0300

    add constructors

--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.RandomAccess;
 
 public class MyArrayList<E> extends AbstractList<E> implements RandomAccess, java.io.Serializable {
@@ -10,14 +12,35 @@ public class MyArrayList<E> extends AbstractList<E> implements RandomAccess, jav
     private static final Object[] DEFAULT_ARRAY = {};
     private static final int MAX_ARRAY_LENGTH = Integer.MAX_VALUE - 8;
 
+    public MyArrayList() {
+        this.elements = DEFAULT_ARRAY;
+    }
 
-    @Override
-    public E get(int index) {
-        return null;
+    public MyArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.elements = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.elements = DEFAULT_ARRAY;
+        } else {
+            throw new IllegalArgumentException("Неверное значение: " + initialCapacity);
+        }
+    }
+
+    public MyArrayList(Collection<? extends E> collection) {
+        Object[] object = collection.toArray();
+        if ((size = object.length) != 0) {
+            elements = Arrays.copyOf(object, size, Object[].class);
+        } else {
+            elements = DEFAULT_ARRAY;
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
+
+
+
+
 }
